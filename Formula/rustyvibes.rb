7d0ff2dc1c9088d64689bdbdd32d4a77f23e0d87
Kunal Bagaria <kb24x7@gmail.com>
2021-09-28 16:14:19 +0530

    Updated Indentation

--- Formula/rustyvibes.rb
+++ Formula/rustyvibes.rb
@@ -1,11 +1,12 @@
 class Rustyvibes < Formula
-    desc "Global mechanical keyboard sound-effects CLI tool"
-    homepage "https://github.com/kb24x7/rustyvibes"
-    url "https://github.com/kb24x7/rustyvibes/releases/download/v1.0.1/rustyvibes-mac.tar.gz"
-    sha256 "41d0e0116a9a9ade346ee4bd4ba776d5e133fff41c249ab10994605cf35aacc9"
-    version "1.0.1"
+  desc "Global mechanical keyboard sound-effects CLI tool"
+  homepage "https://github.com/kb24x7/rustyvibes"
+  url "https://github.com/kb24x7/rustyvibes/releases/download/v1.0.1/rustyvibes-mac.tar.gz"
+  version "1.0.1"
+  sha256 "41d0e0116a9a9ade346ee4bd4ba776d5e133fff41c249ab10994605cf35aacc9"
 
-    def install
-        bin.install "rustyvibes"
-    end
+  def install
+      bin.install "rustyvibes"
+  end
+  
 end
