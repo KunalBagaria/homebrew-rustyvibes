18e3e8a1317509dc7747047c0907185ce4e4c805
Kunal Bagaria <kb24x7@gmail.com>
2021-09-28 12:18:20 +0530

    Fix

--- Formula/rustyvibes.rb
+++ Formula/rustyvibes.rb
@@ -3,7 +3,7 @@ class Rustyvibes < Formula
     homepage "https://github.com/kb24x7/rustyvibes"
     url "https://github.com/kb24x7/rustyvibes/releases/download/v1.0.1/rustyvibes-mac.tar.gz"
     sha256 "41d0e0116a9a9ade346ee4bd4ba776d5e133fff41c249ab10994605cf35aacc9"
-    version "1.0.0"
+    version "1.0.1"
 
     def install
         bin.install "rustyvibes"
