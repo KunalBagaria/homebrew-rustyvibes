7c3ec67de5bc37ce0e63b28109da46eb322c112e
Kunal Bagaria <kb24x7@gmail.com>
2021-10-12 16:20:36 +0530

    Bump Version

--- Formula/rustyvibes.rb
+++ Formula/rustyvibes.rb
@@ -1,9 +1,9 @@
 class Rustyvibes < Formula
   desc "Global mechanical keyboard sound-effects CLI tool"
   homepage "https://github.com/kb24x7/rustyvibes"
-  url "https://github.com/kb24x7/rustyvibes/releases/download/v1.0.7/rustyvibes-mac.tar.gz"
-  version "1.0.7"
-  sha256 "722b92185b7a21c8c0042cec3ccbbf52f6e49866a8e2e60a9cd55096c3145c47"
+  url "https://github.com/kb24x7/rustyvibes/releases/download/v1.0.8/rustyvibes-mac.tar.gz"
+  version "1.0.8"
+  sha256 "54659c261b42cde9c6ed4aa85df62804e61303edf3ec30308a68f8c12fa43d13"
 
   def install
     bin.install "rustyvibes"
